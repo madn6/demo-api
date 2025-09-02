c1cb0d31188627d5739e68626a20a646b0905427
Mathan Muthukani <114805925+madn6@users.noreply.github.com>
2025-09-03 00:09:49 +0530

    Delete another_interview directory

--- another_interview/backend/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { login, register } from '../controller/Auth.controller.js';
-
-const authRouter = Router();
-
-authRouter.post('/register', register);
-authRouter.post('/login', login);
-
-export default authRouter;
